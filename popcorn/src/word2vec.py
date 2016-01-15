d1e265e8f37100824823d144e14009edf6b65701
hkh <guanghao412@gmail.com>
2016-01-15 18:36:13 +0900

    train & test Word2Vec with IMDB movie reviews.

--- popcorn/src/word2vec.py
+++ popcorn/src/word2vec.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 # Import various modules for string cleaning
 from bs4 import BeautifulSoup
 import re
@@ -25,6 +26,56 @@ def review_to_wordlist( review, remove_stopwords=False ):
     # 5. Return a list of words
     return(words)
 
+## words: movie review paragraph
+## model: Word2Vec model
+## num_features: 300 in this case
+def makeFeatureVec(words, model, num_features):
+	# Function to average all of the word vectors in a given paragrah
+	#
+	# Pre-initialize an empty numpy array (for speed)
+	featureVec = np.zeros((num_features,),dtype="float32")
+	#
+	nwords = 0
+	#
+	# Index2word is a list that contains the names of the words in the model's vocabulary.
+	# Convert it to a set, for speed
+	index2word_set = set(model.index2word)
+	#
+	# Loop over each word in the review and, if it is in the model's vocabulary,
+	# add its feature vector to the total
+	for word in words:
+		if word in index2word_set:
+			nwords = nwords + 1
+			featureVec = np.add(featureVec, model[word])
+	#
+	# Divide the result by the number of words to get the average
+	featureVec = np.divide(featureVec, nwords)
+	return featureVec
+
+def getAvgFeatureVecs(reviews, model, num_features):
+	# Given a set of reviews (each one a list of words), caculate
+	# the average feature vector for each one and return a 2D numpy array
+	#
+	# Initialize a counter
+	counter = 0
+	#
+	# Preallocate a 2D numpy array, for speed
+	reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+	#
+	# Loop through the reviews
+	for review in reviews:
+		#
+		# Print a status message every 1000th review
+		if counter%1000 == 0:
+			print "Review %d of %d" % (counter, len(reviews))
+		#
+		# Call the function (defined above) that makes average feature vectors
+		reviewFeatureVecs[counter] = makeFeatureVec(review, model, num_features)
+		#
+		# Increment the counter
+		counter = counter + 1
+
+	return reviewFeatureVecs
 
 # Read data from files
 train = pd.read_csv("/home/hkh/sources/kagglepy/popcorn/data/labeledTrainData.tsv", header=0, delimiter="\t", quoting=3)
@@ -92,5 +143,37 @@ model.init_sims(replace=True)
 
 # It can be helpful to create a meaningful model name and save the model for later use. 
 # You can load it later using Word2Vec.load()
-model_name = "300features_40minwords_10context"
+model_name = "/home/hkh/sources/kagglepy/model/300features_40minwords_10context"
 model.save(model_name)
+
+# *****************************************
+# Calculate average feature vectors for training and testing sets,
+# using the functions we defined above. Notice that we now use stop word removal.
+
+clean_train_reviews = []
+for review in train["review"]:
+	clean_train_reviews.append(review_to_wordlist(review, remove_stopwords=True))
+
+trainDataVecs = getAvgFeatureVecs(clean_train_reviews, model, num_features)
+
+print "Creating average feature vecs for test reviews"
+clean_test_reviews = []
+for review in test["review"]:
+    clean_test_reviews.append(review_to_wordlist(review, remove_stopwords=True))
+
+testDataVecs = getAvgFeatureVecs(clean_test_reviews, model, num_features)
+
+# Fit a random forest to the training data, using 100 trees
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators=100)
+
+print "Fitting a random forest to labeled training data..."
+forest = forest.fit(trainDataVecs, train["sentiment"])
+
+# Text & extract results
+result = forest.predict(testDataVecs)
+
+# Write the test results
+output = pd.DataFrame(data={"id":test["id"], "sentiment":result})
+output.to_csv("/home/hkh/sources/kagglepy/popcorn/output/Word2Vec_AverageVectors.csv", index=False, quoting=3)
+
